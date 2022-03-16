3cf627fbbc23fbf34e314f76b21592dd2dde1e36
zeshhaan <work.zeeshan@gmail.com>
2022-03-16 15:37:36 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vite-vue-element-plus-tailwind
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vite-vue-element-plus-tailwind)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-violet-50 rounded-xl p-5 max-w-md mx-auto">
+    <h1 class="text-violet-500">
+      Vite + Vue + Tailwind + Element Plus Starter
+    </h1>
+  </div>
+</template>
